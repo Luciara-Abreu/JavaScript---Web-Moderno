1c057ad77a9daee045bfd97210c7e14b5a911cfd
Lúci <act113@einstein.br>
2022-07-28 16:30:19 -0300

    while and for  structure repetition

---
+++ 02 - fundamentos/estruturaDeRepeticao/for/index.js
@@ -0,0 +1,17 @@
+/*Vamos ver como fazer com um loop com quantidade s determinadas 
+primeiro exemplo */
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`i: ${i}`);
+}
+
+console.log("--------------------------------");
+
+/*Vamos ver como fazer com percorrer um array 
+segundo exemplo */
+
+const notas = [4.5, 5.5, 6.5, 7.5, 8.5, 9.5, 10.0];
+
+for (let i = 0; i < notas.length; i++) {
+  console.log(`notas: ${notas[i]}`);
+}
