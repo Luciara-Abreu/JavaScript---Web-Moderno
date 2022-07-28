10a055e4c6a38685e9519167e5b0463e6ec6eb65
Lúci <act113@einstein.br>
2022-07-28 16:29:00 -0300

    while structure repetition

--- 02 - fundamentos/estruturaDeRepeticao/while/index.js
+++ 02 - fundamentos/estruturaDeRepeticao/while/index.js
@@ -1,13 +1,22 @@
-function getInteiroAleatório(min, max) {
-  const valor = Math.random() * (min, max) + min;
-  return Math.floor(valor);
+// function getInteiroAleatório(min, max) {
+//   const valor = Math.random() * (min, max) + min;
+//   return Math.floor(valor);
+// }
+
+// let opcao = 0;
+
+// while (opcao != -1) {
+//   opcao = getInteiroAleatório(-1, 10);
+//   console.log(`Opção escolhida foi ${opcao}.`);
+// }
+
+// console.log("Até a próxima");
+
+/*Vamos ver como fazer com um loop com quantidade s determinadas 
+primeiro exemplo */
+
+let contador = 1;
+while (contador <= 10) {
+  console.log(`Contador: ${contador}`);
+  contador++;
 }
-
-let opcao = 0;
-
-while (opcao != -1) {
-  opcao = getInteiroAleatório(-1, 10);
-  console.log(`Opção escolhida foi ${opcao}.`);
-}
-
-console.log("Até a próxima");
