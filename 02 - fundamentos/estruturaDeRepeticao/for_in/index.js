d8b3f42f9d92b700d506b386475fc31ab4593d9d
Lúci <act113@einstein.br>
2022-07-28 16:41:49 -0300

    for in - this code not run

---
+++ 02 - fundamentos/estruturaDeRepeticao/for_in/index.js
@@ -0,0 +1,11 @@
+/*O for in ele não dá as notas do array mas sim o indice desse array*/
+
+const notas = [4.5, 5.5, 6.5, 7.5, 8.5, 9.5, 10.0];
+
+for (i in notas) {
+  console.log(i, notas[i]);
+}
+
+/**
+ * TODO: Só para constar que esse código não rodou ahuahuahuahuha
+ */
