01518445aa03d63cb31d6d7b7905cd6792f57921
Lúci <act113@einstein.br>
2022-07-28 16:01:06 -0300

    continuation of studies

---
+++ 02 - fundamentos/estruturaDeRepeticao/while/index.js
@@ -0,0 +1,13 @@
+function getInteiroAleatório(min, max) {
+  const valor = Math.random() * (min, max) + min;
+  return Math.floor(valor);
+}
+
+let opcao = 0;
+
+while (opcao != -1) {
+  opcao = getInteiroAleatório(-1, 10);
+  console.log(`Opção escolhida foi ${opcao}.`);
+}
+
+console.log("Até a próxima");
