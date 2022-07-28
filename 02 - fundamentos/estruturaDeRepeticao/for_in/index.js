2edb80e4d63b62a23b8cf3d5d5bb5606584392ef
Lúci <act113@einstein.br>
2022-07-28 17:02:40 -0300

    For In / more exemples

--- 02 - fundamentos/estruturaDeRepeticao/for_in/index.js
+++ 02 - fundamentos/estruturaDeRepeticao/for_in/index.js
@@ -1,11 +1,29 @@
 /*O for in ele não dá as notas do array mas sim o indice desse array*/
 
+/**
+ * TODO: Após colocar a saída como o padrão funcionou <3
+ */
+
+console.log("--------------/ FOR / IN /------------------");
+
 const notas = [4.5, 5.5, 6.5, 7.5, 8.5, 9.5, 10.0];
 
 for (i in notas) {
-  console.log(i, notas[i]);
+  console.log(`i= ${notas[i]}`);
 }
 
+console.log("--------------/ Outro exemplo com objeto /------------------");
+
 /**
- * TODO: Só para constar que esse código não rodou ahuahuahuahuha
+ * TODO: Another example
  */
+const pessoa = {
+  name: "Anna",
+  SecundName: "Fracis",
+  age: "33",
+  preferredColor: "000000",
+};
+
+for (atributo in pessoa) {
+  console.log(`${atributo} = ${pessoa[atributo]}`);
+}
