a041724b7a95522a2754272515b44e0866252315
Lúci <act113@einstein.br>
2022-07-29 14:33:32 -0300

    ajustment the fields

---
+++ 03 - estruturasDeControle/break and continue/index.js
@@ -0,0 +1,15 @@
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log("--------------/ Exemplo do break /------------------");
+for (x in nums) {
+  if (x == 5) {
+    break;
+  }
+  console.log(`${x} = ${nums[x]}`);
+}
+console.log("--------------/ Exemplo do continue /------------------");
+for (y in nums) {
+  if (y == 5) {
+    continue;
+  }
+  console.log(`${y} = ${nums[y]}`);
+}
