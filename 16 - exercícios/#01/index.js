e79d55b9003ebfc3c5fd9aa83a863e9cf6524b38
Lúci <act113@einstein.br>
2022-07-30 18:15:26 -0300

    exercicio #01

---
+++ 16 - exercícios/#01/index.js
@@ -0,0 +1,27 @@
+/*
+Crie uma função que dado dois valores (passados como parâmetros) mostre no console 
+a soma, subtração,multiplicação e divisão desses valores.
+*/
+
+function mostreResultados(a, b) {
+  const sum = a + b;
+  const sub = a - b;
+  const mult = a * b;
+  const div = a / b;
+
+    return (
+      `Soma =: ${sum}. ` +
+      `Subtração =: ${sub}. ` +
+      `Multiplicação =: ${mult}. `+
+      `Divisão =: ${div}`
+    )  
+}
+console.log(mostreResultados(6, 3));
+
+
+console.log('---------------------- Correção ----------------------');
+
+function calcularOperacoes (a, b) {
+  console.log('Soma =:', a+b, 'Subtração =:',a-b, 'Multiplicação =:',a*b,'Divisão =:', a/b);  
+}
+calcularOperacoes(6, 3)
