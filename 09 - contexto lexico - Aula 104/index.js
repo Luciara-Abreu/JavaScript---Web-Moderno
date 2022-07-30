e9661ed0c2a02a0285122a50c2bc8c6c534af835
Lúci <act113@einstein.br>
2022-07-30 15:07:51 -0300

    contexto léxico update

--- 09 - contexto lexico - Aula 104/index.js
+++ 09 - contexto lexico - Aula 104/index.js
@@ -10,7 +10,11 @@ function minhaFunction() {
 
 */
 console.log(
-  "-----/ * Mesmo a função 'minhaFunction()' estando dentro de outra função como mostra o exemplo, ao executar,ela trará o valor do contexto em que foi inserida, ou seja ela irá ignorar a const valor = 'Local'e irá imprimir a const valor = 'Global'.. As funções carregam com sigo os locais em que foram definidas* /-----"
+  "Mesmo a função 'minhaFunction()' estando dentro de outra função como"+
+  "mostra o exemplo, ao executar,ela trará o valor do contexto em que foi inserida,"+
+  "ou seja ela irá ignorar a const valor = 'Local'e irá imprimir a"+
+  "const valor = 'Global'.. As funções carregam com sigo os locais em que foram"+
+  "definidas"
 );
 function exec() {
   const valor = "Local";
