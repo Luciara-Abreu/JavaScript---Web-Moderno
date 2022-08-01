a6821461fa83affd06b7e5f262fe2156617c9f35
Lúci <act113@einstein.br>
2022-08-01 12:41:13 -0300

    #4 - Divisão e Resto

---
+++ 16 - exercícios/#04/index.js
@@ -0,0 +1,12 @@
+/**
+04) Crie uma função que irá receber dois valores, o dividendo e o divisor. 
+A função deverá imprimir o resultado
+e o resto da divisão destes dois valores.
+*/
+
+const divisaoEResto=(a,b) => {
+  console.log(resultado = a/b)
+  console.log(`resto = ${a % b}`)  
+}
+
+divisaoEResto(11,4)
