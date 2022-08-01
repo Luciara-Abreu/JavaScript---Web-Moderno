f3af40edc429adda34f7b065bf249043882b734f
Lúci <act113@einstein.br>
2022-08-01 12:21:38 -0300

    #03 - Potenciação

---
+++ 16 - exercícios/#03/index.js
@@ -0,0 +1,10 @@
+/**
+ * 03) Crie uma função que recebe dois parâmetros, base e expoente, e retorne a
+ * base elevada ao expoente.
+ */
+
+const elevadoAPoencia = (x, exp) => {
+  console.log(resultado = x ** exp);
+};
+
+console.log(elevadoAPoencia(3, 3));
