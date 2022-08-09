a838ef62e63fcaeabdcf9dcb36e67515835338b9
Lúci <act113@einstein.br>
2022-08-09 17:25:18 -0300

    #24 - Repetir x vezes com while

---
+++ 16 - exercícios/#24/index.js
@@ -0,0 +1,14 @@
+/*
+Crie um programa que imprima 11 vezes a frase " Hello World!"
+utilizando uma estrutura de repetição while. 
+ */
+
+const repetirWile11 = () => {
+  let x = 1;
+  while (x <= 11) {
+    console.log(x, "Hello World");
+    x++;
+  }  
+};
+
+repetirWile11()
