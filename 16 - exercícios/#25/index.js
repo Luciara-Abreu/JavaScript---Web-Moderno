91ad23c2ed21486d37c667fbd05863818e19a226
Lúci <act113@einstein.br>
2022-08-09 17:29:47 -0300

    #25 - repetir e mostrar na tela com While

---
+++ 16 - exercícios/#25/index.js
@@ -0,0 +1,12 @@
+/**
+ * Escrever um programa para exibir os números de 1 até 50 na tela.
+ */
+
+const repetirAté50 = () =>{
+  x = 1;
+  while(x <=50 ){
+    console.log(x)
+    x++
+  }
+}
+repetirAté50()
