c9e09862775ee7b4137ab443a002b5831ae1a4a3
Lúci <act113@einstein.br>
2022-08-16 16:03:40 -0300

    #01 - introdução OO

---
+++ 01 - Objeto/01 - introducao a OO/index.js
@@ -0,0 +1,31 @@
+// Codigo não executável
+
+// Procedural
+processamento(valor1,valor2,valor3,valor4,valor5)
+
+
+// OO
+objeto = {
+  valor1,
+  valor2,
+  valor3,
+  valor4,
+  valor5,
+  processamento() {
+    //.....
+  },
+};
+
+objeto.processamento();
+
+/* Princípios importantes de oo
+
+1. - Abstração - é vc pegar algo do mundo real e traduzir esse obj para dentro do 
+seu sistema(Atributos e comportamentos)
+2. - Encapsulamento - Que é o que vc precisa saber do programa.
+3. - Herança  (Prototype) - Reutilização de código
+4. - Poliorfismo - Multiplas formas 
+
+
+
+*/
