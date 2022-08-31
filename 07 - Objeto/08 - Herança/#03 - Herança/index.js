3b3ddeb5d1756286914dbb1a0637a6ed08de1da8
Luciara-Abreu <luciara.abreu@gmail.com>
2022-08-31 15:53:20 -0300

    #03 - Herança - Object.keys, For in

--- 07 - Objeto/08 - Herança/#03 - Herança/index.js
+++ 07 - Objeto/08 - Herança/#03 - Herança/index.js
@@ -1,2 +1,31 @@
 console.log("------------------------------------------------");
-console.log('')
+console.log("");
+
+const pai = { nome: "Luci", corCabelo: "Luzes Loiro" };
+
+const filha1 = Object.create(pai);
+filha1.nome = "Maria";
+console.log(`${filha1.nome}, tem cabelo ${filha1.corCabelo}`);
+
+console.log("------------------------------------------------");
+const filha2 = Object.create(pai, {
+  nome: {value:'Bia', writable: false, enumerable: true}
+  // writable = nome não poderá ser alterado 
+  // enumerable = true, é que ele vai ser listado. 
+});
+console.log(`${filha2.nome}, tem cabelo ${filha2.corCabelo}`);
+// vamos tentar mudar o nome da filha 2
+filha2.nome="Carla"
+console.log(filha2.nome) // vimos que writable: false bloqueia mudanças
+
+console.log("------------------------------------------------");
+//Para ver as chaves que os objetos possuem.: 
+console.log(Object.keys(filha1), Object.keys(filha2))
+
+console.log("------------------------------------------------");
+// para listar todos atributos de bjetos podemos fazer assim.:
+// hasOwnProperty = Valida de tal item pertence ao objeto.
+for(let key in filha2){
+  filha2.hasOwnProperty(key) ? 
+      console.log('Tem essa chave: ', key) : console.log(`Por herança tem essa chave: ${key}`)
+}
