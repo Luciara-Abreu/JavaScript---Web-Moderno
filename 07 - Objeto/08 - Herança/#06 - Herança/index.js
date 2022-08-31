cec068adbf906260bcfe51919a851f18d8ed40ef
Luciara-Abreu <luciara.abreu@gmail.com>
2022-08-31 17:24:21 -0300

    #06 - Herança - O que acontece ao usar o op new

--- 07 - Objeto/08 - Herança/#06 - Herança/index.js
+++ 07 - Objeto/08 - Herança/#06 - Herança/index.js
@@ -1,2 +1,27 @@
 console.log("------------------------------------------------");
-console.log('')
+console.log("");
+
+// função construtora
+function Aula(nome, videoID) {
+  this.nome = nome;
+  this.videoID = videoID;
+}
+//instanciando o obj
+const aula1 = new Aula("Bem vindo a aula de JS", 123);
+const aula2 = new Aula("Bem vindo a aula de React", 456);
+console.log(aula1, aula2);
+console.log("------------------------------------------------");
+console.log("Simulação para ver o que acontece quando usamos o operador new");
+//Criar o proprio new
+// Esse obj será criado apartir da função construtora Aula
+function novoNew(f, ...params) {
+  const obj = {};
+  obj.__proto__ = f.prototype;
+  f.apply(obj, params);
+  return obj;
+}
+const aula3 = new novoNew(Aula, "Bem vindo a aula de JS", 123);
+const aula4 = new novoNew(Aula, "Bem vindo a aula de React", 456);
+console.log(aula3, aula4);
+
+console.log("Obrigado New por já existir kkkkkkkk :) ");
