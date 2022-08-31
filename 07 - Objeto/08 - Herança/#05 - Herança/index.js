2ebdae68fd5d22b02461bd529e6fcc50c26b80ce
Luciara-Abreu <luciara.abreu@gmail.com>
2022-08-31 17:00:35 -0300

    #05 - Herança - Mais sobre prototype

--- 07 - Objeto/08 - Herança/#05 - Herança/index.js
+++ 07 - Objeto/08 - Herança/#05 - Herança/index.js
@@ -1,2 +1,19 @@
 console.log("------------------------------------------------");
-console.log('')
+console.log('What is typeof String? Typeof String is a', typeof String)
+console.log('What is typeof Array? Typeof Array is a',typeof Array)
+console.log('What is typeof Object? Typeof Object is a ',typeof Object)
+console.log('Are all functions')
+console.log("------------------------------------------------");
+// Toda function tem o prototype então podemos fazer coisas do tipo
+String.prototype.reverse = function(){
+  return this.split('').reverse().join('')
+}
+console.log('Luci está aprendendo JS'.reverse())
+console.log("------------------------------------------------");
+
+// Não temos esse metodo first mas da para add
+Array.prototype.first = function(){
+  return this[3]
+}
+console.log([1,2,3,4,5].first())
+console.log(['a','b','c','d','e'].first())
