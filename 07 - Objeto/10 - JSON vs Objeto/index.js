80c5833a5b1ea4375d3bdecdfc7c230aa6c62d0e
Luciara-Abreu <luciara.abreu@gmail.com>
2022-09-03 16:42:24 -0300

    #10 - JSON vs Objeto

--- 07 - Objeto/10 - JSON vs Objeto/index.js
+++ 07 - Objeto/10 - JSON vs Objeto/index.js
@@ -1,4 +1,7 @@
 console.log("------------------------------------------------");
 console.log("JSON vc Objeto");
-const obj = {a: 1, b: 2, c: 3, soma(){ return a + b + c}}
+const obj = {a: 1, b: 2, c: 3, soma(){ return a + b + c}}//não faz calculos
 console.log(JSON.stringify(obj))
+
+console.log(JSON.parse('{"a":1, "b":2, "C":3}'))
+console.log(JSON.parse('{"a":1, "b":2, "C":3, "d": {}, "e": []}'))
