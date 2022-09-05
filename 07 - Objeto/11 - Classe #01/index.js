ed0d5628072234b5f3dc7125ddc36cf9819a1820
Luciara-Abreu <luciara.abreu@gmail.com>
2022-09-05 16:17:55 -0300

    #11 - Classe #01

--- 07 - Objeto/11 - Classe #01/index.js
+++ 07 - Objeto/11 - Classe #01/index.js
@@ -1,2 +1,35 @@
 console.log("------------------------------------------------");
 console.log("");
+class Lancamento {
+  constructor (nome = 'Generico',valor = 0) {
+      this.nome = nome
+      this.valor = valor
+  }
+}
+
+class CicloFinanceiro {
+  constructor(mes, ano){
+    this.mes = mes
+    this.ano = ano
+    this.lancamentos = []
+  }
+  addLancamentos(...lancamentos){
+    lancamentos.forEach(l => this.lancamentos.push(l))
+  }
+  sumario(){
+    let valorConsolidado = 0
+    this.lancamentos.forEach(l => {
+      valorConsolidado += l.valor
+    })
+    return valorConsolidado
+  }
+}
+const salario = new Lancamento('Salario', 45000)
+const contaDeLuz = new Lancamento('Luz', -420)
+
+const contas = new CicloFinanceiro(6, 2022)
+contas.addLancamentos(salario, contaDeLuz)
+
+console.log("------------------------------------------------");
+console.log(contas.sumario())
+
