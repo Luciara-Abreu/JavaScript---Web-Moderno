1a6c12c095be252fdd289f3522a5ffdf1a777b0e
Luciara-Abreu <luciara.abreu@gmail.com>
2022-09-06 13:17:57 -0300

    #12 - Classes #02

--- 07 - Objeto/12 - Classe #02/index.js
+++ 07 - Objeto/12 - Classe #02/index.js
@@ -1,2 +1,25 @@
 console.log("------------------------------------------------");
 console.log("");
+
+class Avo {
+  constructor(sobrenome) {
+    this.sobrenome = sobrenome
+  }
+}
+
+class Pai extends Avo {
+  constructor(sobrenome, profissao = 'Professor'){
+    super(sobrenome)
+    this.sobrenome = sobrenome
+    this.profissao = profissao
+  }
+}
+
+class Filho extends Pai {
+  constructor(){
+    super('Abreu')
+  }
+}
+
+const filho = new Filho()
+console.log(filho)
