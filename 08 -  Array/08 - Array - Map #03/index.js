004ade54ceb079da3c92c4497a4e3557cbd4115c
Luciara-Abreu <luciara.abreu@gmail.com>
2022-09-09 21:43:39 -0300

    08 - Array - Implementar o Map #03

--- 08 -  Array/08 - Array - Map #03/index.js
+++ 08 -  Array/08 - Array - Map #03/index.js
@@ -1,2 +1,29 @@
 console.log("------------------------------------------------");
-console.log("");
+console.log("Implementar o Map para ver como ele funciona - Map #03");
+
+
+Array.prototype.map2 = function(callback) { 
+  const newArray = []
+    for (let i = 0; i < this.length; i++) {
+      newArray.push(callback(this[i], i, this)) 
+  }
+  return newArray
+}
+
+const carrinho = [
+  '{"nome": "Borracha", "preco": 3.45}',
+  '{"nome": "Caderno", "preco": 13.90}',
+  '{"nome": "kit de Lapis", "preco": 41.22}',
+  '{"nome": "Caneta", "preco": 7.50}'
+]
+
+//Retornar um array apenas com os preços
+
+const paraObjeto = json => JSON.parse(json)
+const apenasPreco = value => value.preco
+/*
+com esses dois metodos criados a cima, podemos chamar dois maps e trazer só o
+dado nescessário
+*/ 
+const result = carrinho.map2(paraObjeto).map2(apenasPreco)
+console.log("Apenas os preços = >",result)
