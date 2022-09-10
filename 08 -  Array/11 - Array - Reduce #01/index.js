99aaf2c20615b135e144e01ea0629a317b4f8741
Luciara-Abreu <luciara.abreu@gmail.com>
2022-09-09 23:16:52 -0300

    11 - Array - Reduce #01

--- 08 -  Array/11 - Array - Reduce #01/index.js
+++ 08 -  Array/11 - Array - Reduce #01/index.js
@@ -2,17 +2,20 @@ console.log("------------------------------------------------");
 console.log("Array - Reduce #01");
 
 const alunos = [
-  {nome: 'João', nota: 7.3, bolsista: false},
-  {nome: 'Anna', nota: 9.2, bolsista: true},
-  {nome: 'Lucas', nota: 9.8, bolsista: true},
-  {nome: 'Luci', nota: 8.7, bolsista: false}
-]
+  { nome: "João", nota: 7.3, bolsista: false },
+  { nome: "Anna", nota: 9.2, bolsista: true },
+  { nome: "Lucas", nota: 9.8, bolsista: true },
+  { nome: "Luci", nota: 8.7, bolsista: false },
+];
 
-const resultado = alunos.map(a => a.nota).reduce(function(acumulador, atual){
-console.log(acumulador, atual)
-return acumulador + atual
-},0)
+const resultado = alunos
+  .map((a) => a.nota)
+  .reduce(function (acumulador, atual) {
+    console.log(acumulador, atual);
+    return acumulador + atual;
+  }, 0);
 
-//console.log(alunos.map(a => a.nota))
 console.log("------------------------------------------------");
-console.log(resultado)
+console.log(alunos.map((a) => a.nota));
+console.log("------------------------------------------------");
+console.log(resultado);
