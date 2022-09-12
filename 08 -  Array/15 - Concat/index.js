84db5712b8da1a5c3861655d3d60bc13063a075a
Luciara-Abreu <luciara.abreu@gmail.com>
2022-09-12 18:37:46 -0300

    15 - Array - Concat

--- 08 -  Array/15 - Concat/index.js
+++ 08 -  Array/15 - Concat/index.js
@@ -1,2 +1,13 @@
 console.log("------------------------------------------------");
-console.log("");
+console.log("Array - Concat");
+
+const filhas = ['Luci','Viviane','Daiane']
+const filhos = ['Eder', 'Cristiano', 'Luiz']
+const todos = filhas.concat(filhos)
+const todos2 = filhas.concat(filhos,'Nayelle','Bruna','Lucas','Tiago','Marcelo')
+
+
+console.log(filhas)
+console.log(filhos)
+console.log(todos)
+console.log(todos2)
