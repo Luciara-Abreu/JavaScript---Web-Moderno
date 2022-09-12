17e07871123bf58c670431a606ba2c601e366c61
Luciara-Abreu <luciara.abreu@gmail.com>
2022-09-12 19:32:51 -0300

    16 - Array - FlatMap

--- 08 -  Array/16 - FlatMAp/index.js
+++ 08 -  Array/16 - FlatMAp/index.js
@@ -1,2 +1,53 @@
 console.log("------------------------------------------------");
-console.log("");
+console.log("Array - FlatMap");
+
+const escola = [
+  {
+    nome: 'Turma M1',
+    alunos: [
+      {
+      nome: 'Gustavo',
+      nota:8.1
+      },
+      {
+      nome: 'Anna',
+      nota:9
+      },
+      {
+      nome: 'Lucas',
+      nota:9.1
+      }]
+  },    
+  {
+    nome: 'Turma M2',
+    alunos: [
+      {
+        nome: 'Rebeca',
+        nota:7.3
+      },
+      {
+        nome: 'Roberto',
+        nota: 8.9
+      },
+      {
+        nome: 'Carla',
+        nota: 7.6
+      }]
+  }
+]
+
+const getNotaAluno = aluno => aluno.nota
+const getNotasDaTurma = turma => turma.alunos.map(getNotaAluno)
+
+const notas1 = escola.map(getNotasDaTurma)
+console.log(notas1)
+
+console.log("------------------------------------------------");
+console.log([].concat([8.7,9.3], [8.9,7.3]))
+
+Array.prototype.flatMap = function(callback){
+  return Array.prototype.concat.apply([], this.map(callback))
+}
+
+const nota2 = escola.flatMap(getNotasDaTurma)
+console.log(nota2)
