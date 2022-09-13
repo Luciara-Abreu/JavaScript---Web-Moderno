081c8de71de07ef9144857c6e6f881df5ee2dcc8
Luciara-Abreu <luciara.abreu@gmail.com>
2022-09-13 08:03:32 -0300

    #02 - Receber idade e transformar em qt de dias

---
+++ 08 -  Array/Exercícios/#02 - exe/index.js
@@ -0,0 +1,15 @@
+console.log("------------------------------------------------");
+console.log("Exercícios");
+
+console.log("------------------------------------------------");
+/*2) Escreva uma função que receba a idade uma uma pessoa em anos
+e retorne a mesma idade em dias
+*Considerando que o ano tenha 365 dias
+*/
+
+const idade = 0
+
+const idadeEmDias = (idade) => {
+  return idade * 365
+}
+console.log('Você já viveu cerca de',idadeEmDias( 45) ,'dias!')
