dc2365661d286908389c234d86a6f724a785a533
Luciara-Abreu <luciara.abreu@gmail.com>
2022-09-27 14:54:46 -0300

    #12 - exe

---
+++ 08 -  Array/Exercícios/#12 - exe/index.js
@@ -0,0 +1,27 @@
+console.log("------------------------------------------------");
+/*12)
+* Desenvolva uma função que receba um objeto e retorne um array
+de arrays.
+*Cada elemento é um array formado pelos pares chave/valor que 
+*/
+console.log("------------------------------------------------");
+
+const dadosPessoa = ({
+  nome: "Luci Abreu",
+  profissão: "Desenvolvedora de software" 
+})
+
+const arrayPessoa =(dadosPessoa) =>{
+  const result =[]
+
+  for( let chave in dadosPessoa)
+  result.push([chave, dadosPessoa[chave]])
+  
+  return result
+}
+
+console.log(arrayPessoa(dadosPessoa))
+
+console.log("------------------------------------------------");
+console.log("Exemplo profe");
+
