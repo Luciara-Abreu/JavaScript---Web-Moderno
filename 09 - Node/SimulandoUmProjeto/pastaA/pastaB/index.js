88c5433cb0f85e7dd8335c4f1623d51ced5b782f
Luciara-Abreu <luciara.abreu@gmail.com>
2022-10-04 12:21:49 -0300

    Node - Require

---
+++ 09 - Node/SimulandoUmProjeto/pastaA/pastaB/index.js
@@ -0,0 +1,9 @@
+const moduloA = require('../../../conceitoDeModulo/moduloA');
+console.log(moduloA.ola);
+console.log("------------------------------------------------");
+
+const c = require('./pastaC')
+console.log(c.ola2)
+
+console.log("------------------------------------------------");
+
