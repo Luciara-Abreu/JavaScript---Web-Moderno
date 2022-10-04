a6804025f558a0e1f98ae9d752e4196a8cad3ce1
Luciara-Abreu <luciara.abreu@gmail.com>
2022-10-04 14:19:37 -0300

    Node - Desafio

--- 09 - Node/SimulandoProjetoFuncionarios/funcionarios/fucionarios.js
+++ 09 - Node/SimulandoProjetoFuncionarios/funcionarios/fucionarios.js
@@ -1,10 +1,35 @@
-const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
+const url = "http://files.cod3r.com.br/curso-js/funcionarios.json";
 console.log("------------------------------------------------");
 console.log("O axios é um claint que faz requisições HTTP");
-const axios = require('axios')
+const axios = require("axios");
 
-axios.get(url).then(response => {
-  const funcionarios = response.data
-  console.log(funcionarios)
+console.log(
+  "para resolver o desafio para trazer uma mulher",
+  "chinesa com menor salário usaremos tres funções"
+);
+console.log("------------------------------------------------");
+console.log("filter");
+const chineses = (f) => f.pais === "China";
+const mulheres = (f) => f.genero === "F";
 
-})
+console.log("reduce");
+const menorSalario = (func, funcAtual) => {
+  return func.salario < funcAtual.salario ? func : funcAtual;
+};
+
+console.log("------------------------------------------------");
+//AQUI TRAS A LISTA
+axios.get(url).then((response) => {
+  const funcionarios = response.data;
+  //console.log(funcionarios)
+
+  console.log("------------------------------------------------");
+  //AQUI TRAREMOS DOS DADOS DO EXERCÍCIO
+
+  const func = funcionarios
+    .filter(chineses)
+    .filter(mulheres)
+    .reduce(menorSalario);
+
+  console.log(func);
+});
